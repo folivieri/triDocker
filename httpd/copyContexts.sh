476f716cd07ffb5a3d3a20804b7bbb12076b0c42
Frederic Olivieri <folivieri@mc02tt8nshtd5.ad.corp.local>
2018-03-22 11:45:25 -0700

    Modified for our webapp

---
+++ httpd/copyContexts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp /opt/trinet/trinetGateway.xml /usr/local/tomcat/webapps/trinetGateway/META-INF/context.xml
+
+cp /opt/trinet/TriNet.xml /usr/local/tomcat/webapps/TriNet/META-INF/context.xml
