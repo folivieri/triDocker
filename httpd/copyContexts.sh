489ff351c8fc002ffe4e5192a589b220bd7b7a0c
Frederic Olivieri <folivieri@mc02tt8nshtd5.ad.corp.local>
2018-03-22 11:49:30 -0700

    Removed extraneous files

--- httpd/copyContexts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cp /opt/trinet/trinetGateway.xml /usr/local/tomcat/webapps/trinetGateway/META-INF/context.xml
-
-cp /opt/trinet/TriNet.xml /usr/local/tomcat/webapps/TriNet/META-INF/context.xml
